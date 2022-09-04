5c3fb1cdb7d58f078b019efbcbf005d088e4e7fd
Vo Van Thuong <vovanthuong@outlook.com.vn>
2022-09-04 12:01:00 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,89 @@
+'use strict'
+
+var express = require('express');
+var router = express.Router();
+const multer = require('multer');
+// Luu vo o cung
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads') 
+    },
+    filename: function (req, file, cb) {
+      const filename = Date.now() + '-' + Math.round(Math.random() * 1E9) 
+      cb(null, filename + '-' + file.originalname )
+    }
+  })
+// Luu vo ram
+var memory = multer.memoryStorage();
+const upload = multer({ storage: memory });
+
+const libre = require('libreoffice-convert');
+const path = require('path');
+libre.convertAsync = require('util').promisify(libre.convert);
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/',  upload.single('document'), async function(req, res, next) {
+    const file = req.file;
+    if (!file) {
+        const error = new Error('Upload file again!');
+        error.httpStatusCode = 400;
+        return next(error);
+      }
+    const originalExt = path.extname(file.originalname);
+    const destinationExt = '.pdf';
+    let originalName = file.originalname.substring(0, file.originalname.length - originalExt.length);
+    let pdfBuf = await libre.convertAsync(file.buffer, destinationExt, undefined);
+    res.setHeader('status', 200);
+    res.setHeader('Content-Type', 'application/pdf');
+    // res.setHeader('Content-disposition', 'inline; filename="' + originalName + destinationExt + '"');
+    res.send(pdfBuf);
+});
+
+router.post('/option1',  upload.single('document'), async function(req, res, next) {
+    const file = req.file;
+    if (!file) {
+        const error = new Error('Upload file again!');
+        error.httpStatusCode = 400;
+        return next(error);
+      }
+    const originalExt = path.extname(file.originalname);
+    const destinationExt = '.pdf';
+    let originalName = file.originalname.substring(0, file.originalname.length - originalExt.length);
+    
+    let pdfBuf = await libre.convertAsync(file.buffer, destinationExt, undefined);
+    res.setHeader('status', 200);
+    res.setHeader('Content-Type', 'application/pdf');
+    // res.setHeader('Content-disposition', 'inline; filename="' + originalName + destinationExt + '"');
+    res.send(pdfBuf);
+});
+
+/* GET home page. */
+router.get('/option2', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/',  upload.single('document'), async function(req, res, next) {
+    const file = req.file;
+    if (!file) {
+        const error = new Error('Upload file again!');
+        error.httpStatusCode = 400;
+        return next(error);
+      }
+    const originalExt = path.extname(file.originalname);
+    const destinationExt = '.pdf';
+    let originalName = file.originalname.substring(0, file.originalname.length - originalExt.length);
+    
+    let pdfBuf = await libre.convertAsync(file.buffer, destinationExt, undefined);
+    res.setHeader('status', 200);
+    // res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-disposition', 'inline; filename="' + originalName + destinationExt + '"');
+    res.send(pdfBuf);
+});
+
+module.exports = router;
